02c9350cf52763608217de77ef87135263412c7c
michalliss <michal.j.liss@gmail.com>
2022-11-06 15:30:05 +0100

    Update Main.scala

--- src/main/g8/src/main/scala/$package$/Main.scala
+++ src/main/g8/src/main/scala/$package$/Main.scala
@@ -4,12 +4,12 @@ import zio._
 import main.services.IEmptyService
 
 object Main extends ZIOAppDefault:
-  
+
   lazy val app =
     for
       service <- ZIO.service[IEmptyService]
       result  <- service.doWork()
-      _       <- Console.printLine(s"Result: $result")
+      _       <- Console.printLine(s"Result: ${result}")
     yield ()
 
   def run = app.provide(IEmptyService.live)
